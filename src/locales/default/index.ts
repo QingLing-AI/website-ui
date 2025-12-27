3271328b705e284a62a46b9ad2b3851c235e61dd
hua <lshgdut@qq.com>
2025-12-27 16:39:25 +0800

    feat(locales): 添加专家模块国际化资源
    新增expert模块的国际化配置，并将其集成到默认资源中

--- src/locales/default/index.ts
+++ src/locales/default/index.ts
@@ -1,11 +1,13 @@
 import common from './common';
 import error from './error';
+import expert from './expert';
 import phd_prog from './phd-prog';
 
 const resources = {
   common,
   error,
   phd_prog,
+  expert,
 } as const;
 
 export default resources;
